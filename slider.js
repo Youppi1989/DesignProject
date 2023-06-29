7cc9bab6527bc6d21a2b4c67effe5121b7d3496f
Veronika <trotsenko_nika@mail.ru>
2023-06-29 12:40:42 +0200

    in progress

--- slider.js
+++ slider.js
@@ -4,7 +4,7 @@ const entities = [
     text2: "81 m2",
     text3: "3.5 months",
     text4: "Upon request",
-    backgroundImage: "./img/image 2.1.png",
+    image: "./img/image 2.1.png",
     menuLiA: "ROSTOV-ON-DON, ADMIRAL",
   },
   {
@@ -12,7 +12,7 @@ const entities = [
     text2: "105 m2",
     text3: "4 months",
     text4: "Upon request",
-    backgroundImage: "./img/Mask Group.jpg",
+    image: "./img/Mask Group.jpg",
     menuLiA: "Sochi Thieves",
   },
   {
@@ -20,7 +20,7 @@ const entities = [
     text2: "93 m2",
     text3: "3 months",
     text4: "Upon request",
-    backgroundImage: "./img/Mask Group (1).png",
+    image: "./img/Mask Group (1).png",
     menuLiA: "Rostov-on-Don Patriotic",
   },
 ];
@@ -30,7 +30,10 @@ const arrowNext = document.querySelector(".next");
 const dots = document.querySelectorAll(".dot");
 const titleElement = document.querySelector(".text1");
 const textElement = document.querySelector(".text2");
+const titlElement = document.querySelector(".text3");
+const texElement = document.querySelector(".text4");
 const menuLinks = document.querySelectorAll(".menu_li_a");
+const imageElement = document.querySelector(".image");
 
 let currentIndex = 0;
 
@@ -39,8 +42,10 @@ function updateSlide(index) {
     dot.classList.toggle("active", i === index);
   });
 
-  titleElement.textContent = titles[index];
-  textElement.textContent = descriptions[index];
+  titleElement.innerHTML = entities[index].text1;
+  textElement.innerHTML = entities[index].text2;
+  titlElement.innerHTML = entities[index].text3;
+  texElement.innerHTML = entities[index].text4;
 
   menuLinks.forEach((link, i) => {
     link.classList.toggle("active", i === index);
@@ -49,14 +54,12 @@ function updateSlide(index) {
 
 function showSlide(index) {
   if (index < 0) {
-    currentIndex = images.length - 1;
-  } else if (index >= images.length) {
+    currentIndex = entities.length - 1;
+  } else if (index >= entities.length) {
     currentIndex = 0;
   }
 
-  document.querySelector(
-    ".image"
-  ).style.backgroundImage = `url(${images[currentIndex]})`;
+  imageElement.style.backgroundImage = `url(${entities[index].image})`;
 
   updateSlide(currentIndex);
 }
